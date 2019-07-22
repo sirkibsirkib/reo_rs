6b02b530953ba4ec31d65bf783e7d7a89bb22e77
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-07-22 17:26:55 +0200

    yea

--- src/tests.rs
+++ src/tests.rs
@@ -578,6 +578,7 @@ fn pos_neg_claim() {
 
 #[test]
 fn pos_neg_classification() {
+    use {Instruction::*, Term::*, NameDef::*}; 
     let p = POS_NEG.build(MemInitial::default()).unwrap();
     let (mut p, mut cpos, mut cneg): (Putter<i32>, Getter<i32>, Getter<i32>) = (
         Putter::claim(&p, "P").unwrap(),
