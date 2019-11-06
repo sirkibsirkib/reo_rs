0f21e50eee280eb520902f21c908b170b752aeeb
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-11-06 13:11:46 +0100

    new api

--- mine.h
+++ mine.h
@@ -1,9 +1,18 @@
-#include <cstdint>
+#include <stdint.h>
 
 typedef struct CProtoHandle {
-	inner: uintptr_t;
+  uintptr_t inner;
 } CProtoHandle;
 
 CProtoHandle reors_empty_proto_create();
+void reors_proto_handle_destroy(CProtoHandle*);
 
-void reors_proto_handle_destroy(CProtoHandle *);
+typedef struct CPutter {
+  uintptr_t: locid;
+  uintptr_t: tid;
+  uintptr_t: handle;
+} CPutter;
+
+CPutter reors_putter_claim(CProtoHandle*, char*);
+bool reors_putter_put_raw(CPutter*, void**);
+reors_putter_destroy(CPutter*);
