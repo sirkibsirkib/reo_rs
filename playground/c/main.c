74206bde2a456787e7f9b0eeb2a9fe803b095f9e
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-11-06 17:36:51 +0100

    fix

--- playground/c/main.c
+++ playground/c/main.c
@@ -3,7 +3,10 @@
 #include "assert.h"
 
 int main() {
+	// create a protocol instance
 	CProtoHandle proto = proto_create();
+
+	// claim, initialize the logical ports "A" and "B" and bind them to variables.
 	CPutter a = reors_putter_claim(&proto, "A");
 	CGetter b = reors_getter_claim(&proto, "B");
 
@@ -13,19 +16,21 @@ int main() {
 	void* from = (void*) &value; // indirection read by reo_rs
 	void* to;                    // indirection written by reo_rs
 
-	// send the message through the fifo buffer slot
+	// send the message from A to B py interacting with ports.
+	/* put, get, destroy, claim functions are all thread-safe UNLESS they are
+	   all performed on the same PORT. eg: `a` and `b` can be on different threads */
 	reors_putter_put_raw(&a, &from);
 	reors_putter_get_raw(&b, &to);
 
+	// "local" computation of B. 
 	value2 = *((int *) to);
 	assert(value == value2);
 
-	// cleanup
+	// cleanup. destroy ports and protocol handle (order is irrelevant).
 	reors_putter_destroy(&a);
 	reors_getter_destroy(&b);
 	reors_proto_handle_destroy(&proto);
 
 	printf("Everything went fine\n");
-
 	return 0;
 }
--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -1,27 +1,36 @@
 mod protocol;
+use reo_rs::{Putter, Getter};
 
 fn main() {
+	// create a protocol instance
 	let proto = protocol::new_fifo1();
 	
-	use reo_rs::{Putter, Getter};
+	// claim, initialize the logical ports "A" and "B" and bind them to variables.
+	/* Rust's borrowing rules will ensure only safe behavior is allowed.
+	   Note that it is permitted to move port objects between threads to
+	   achieve concurrency / parallelism. */
 	let mut a = Putter::<isize>::claim(&proto, "A").unwrap();
 	let mut b = Getter::<isize>::claim(&proto, "B").unwrap();
 
 	let to = std::time::Duration::from_millis(500);
 	
+	// send the number 5 from A to B
 	a.put(5);
 	let x = b.get();
 	assert_eq!(x, 5);
 	println!("Sent {}", x);
 	
+	// send the number 2 from A to B, where B will consider timing out
 	a.put(2);
 	let x = b.get_timeout(to).unwrap();
 	assert_eq!(x, 2);
 	println!("Sent {}", x);
 
-	// times out! No data is waiting!
+	// B tries to get data from the protocol's buffer slot but times out.
+	// changing this to `b.get()` causes deadlock; `get` blocks UNTIL it succeeds.
 	assert!(b.get_timeout(to).is_none()); 
 	println!("Timed out!");
 	
 	println!("Everything went fine!");
+	// {a, b, proto} go out of scope and are cleaned up safely.
 }
