d2af0e6eb2d08178f9e85d0f41baab1f9574c5c0
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2021-09-02 17:58:04 +0200

    a more thorough test

--- src/new_tests.rs
+++ src/new_tests.rs
@@ -16,6 +16,7 @@ fn main() {
 			"A" => NameDef::Port { is_putter: true, type_key: TypeKey(0), },
 			"B" => NameDef::Mem {  type_key: TypeKey(0), },
 			"C" => NameDef::Port { is_putter: false, type_key: TypeKey(0), },
+			"D" => NameDef::Port { is_putter: false, type_key: TypeKey(0), },
 		},
 		rules: vec![
 			RuleDef {
@@ -40,18 +41,34 @@ fn main() {
 					"B" => (false, hashset!{"C"}),
 				},
 			},
+			RuleDef {
+				state_guard: StatePredicate {
+					ready_ports: hashset!{"D"},
+					full_mem: hashset!{"B"},
+					empty_mem: hashset!{},
+				},
+				ins: vec![],
+				output: hashmap!{
+					"B" => (false, hashset!{"D"}),
+				},
+			},
 		],
 	};
 	let p = proto_def.build(type_map).unwrap();
 	unsafe { p.fill_memory("B", TypeKey(0), true) }.unwrap();
 
-    let (mut p, mut g): (Putter<bool>, Getter<bool>) = unsafe {
-        (Putter::claim(&p, "A", TypeKey(0)).unwrap(), Getter::claim(&p, "C", TypeKey(0)).unwrap())
+    let (mut a, mut c, mut d): (Putter<bool>, Getter<bool>, Getter<bool>) = unsafe {
+        (
+        	Putter::claim(&p, "A", TypeKey(0)).unwrap(),
+        	Getter::claim(&p, "C", TypeKey(0)).unwrap(),
+        	Getter::claim(&p, "D", TypeKey(0)).unwrap()
+        )
     };
 
 	let handles = [
-		thread::spawn(move || {p.put(true);p.put(false);}),
-		thread::spawn(move || {println!("{:?}", g.get()); println!("{:?}", g.get()); println!("{:?}", g.get()); }),
+		thread::spawn(move || for _ in 0..3 {a.put(true);}),
+		thread::spawn(move || for _ in 0..2 {println!("C {:?}", c.get());}),
+		thread::spawn(move || for _ in 0..2 {println!("D {:?}", d.get());}),
 	];
 	for h in handles {
 		h.join().unwrap();
